0c6ed9321d1070c1206b316d15a730bf7a4ab73f
susu <dsusu7986@163.com>
2021-02-18 17:28:47 +0800

    star.png

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,8 +25,8 @@ export default {
   data() {
     return {
       title: 'Pano Admin 控制台',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
-      // logo: '@/assets/star.ico'
+      // logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: require('@/assets/star.png')
     }
   }
 }
